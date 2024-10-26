9014200a66c5a8f97fe4811be7944840b459c7c1
emerson <emersoneletrotecnico2013@hotmail.com>
2024-10-26 14:56:11 -0300

    verbose_name
    colocando os nomes no plural

--- contact/models.py
+++ contact/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from django.utils import timezone
 
 class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+        
     name = models.CharField(max_length=50)
 
     def __str__(self):
@@ -17,7 +21,7 @@ class Contact(models.Model):
     show = models.BooleanField(default=True)
     picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
-    
+
     
     # owner
    
