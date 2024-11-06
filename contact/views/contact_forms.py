d365addc42bc35664621b9ef00ffd71c16ed0c7e
emerson <emersoneletrotecnico2013@hotmail.com>
2024-11-06 14:18:38 -0300

    form
    furmulario dinamico 2 parte

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render
 from contact.models import Contact
 from django import forms
+from django.core.exceptions import ValidationError
 
 class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
         fields = ('first_name', 'last_name', 'phone')
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+
+        self.add_error('first_name', ValidationError('mensagem de erro', code='invalid'))
+        return super().clean()   
+    
+    
+    def clean(self):
+        cleaned_data = self.cleaned_data
+
+        self.add_error('first_name', ValidationError('mensagem de erro 2', code='invalid'))
+        return super().clean() 
 
 def create(request):
     if request.method == 'POST':
